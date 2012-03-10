708472645ffea29d219f98e1c06f45d6d98c18ea
Yong Bakos <ybakos@humanoriented.com>
2012-03-10 10:52:43 -0700

    Transcribed FileManager constructor. Tests pass.

--- lib/rudbms/file/file_manager.rb
+++ lib/rudbms/file/file_manager.rb
@@ -1,2 +1,42 @@
+# The SimpleDB file manager.
+# The database system stores its data as files within a specified directory.
+# The file manager provides methods for reading the contents of a file block
+# to a Java byte buffer, writing the contents of a byte buffer to a file block,
+# and appending the contents of a byte buffer to the end of a file.
+# These methods are called exclusively by the class <tt>Page</tt>, and are
+# thus package-private.
+# The class also contains two public methods:
+# <tt>isNew()</tt> is called during system initialization by <tt>SimpleDB#init</tt>.
+# <tt>size(String)</tt> is called by the log manager and transaction manager to
+# determine the end of the file.
+# (Edward Sciore)
 class FileManager
+
+  TEMP_FILE_PREFIX = "temp"
+
+  attr_reader :is_new; alias_method :is_new?, :is_new
+
+  # Creates a file manager for the specified database.
+  # The database will be stored in a folder of that name in the user's home directory.
+  # If the folder does not exist, then a folder containing an empty database
+  # is created automatically.
+  # Files for all temporary tables (i.e. tables beginning with "temp") are deleted.
+  # <tt>dbname</tt>: the name of the directory that holds the database.
+  def initialize(dbname)
+    @open_files = Hash.new
+    dbpath = "#{Dir.pwd}/#{dbname}"
+    # TODO: I don't like this.
+    unless File.exists?(dbpath)
+      Dir.mkdir(dbpath)
+      @is_new = true
+    else
+      @is_new = false
+    end
+    @db_directory = File.new(dbpath)
+    # remove any leftover temporary tables
+    Dir.foreach("#{dbpath}") do |f|
+      FileUtils.rm("#{dbpath}/#{f}") if f.start_with? TEMP_FILE_PREFIX
+    end
+  end
+
 end
--- test/test_file_manager.rb
+++ test/test_file_manager.rb
@@ -2,4 +2,49 @@ require_relative 'helper'
 require_relative '../lib/rudbms/file/file_manager'
 
 class TestFileManager < MiniTest::Unit::TestCase
+
+  def setup
+    @dbname = 'testdb'
+  end
+
+  def teardown
+    FileUtils.rm_rf(@dbname) if File.exists?(@dbname)
+  end
+
+  def test_must_initialize_with_dbname
+    assert_raises(ArgumentError) do
+      FileManager.new
+    end
+    assert FileManager.new(@dbname), 'Could not instantiate FileManager with one param.'
+  end
+
+  def test_has_accessor_is_new?
+    FileManager.new(@dbname).respond_to? :is_new?
+  end
+
+  def test_initialization_should_create_new_directory_when_nonexistent
+    refute File.exists?(@dbname)
+    f = FileManager.new(@dbname)
+    assert File.exists?(@dbname), "FileManager did not create directory #{@dbname}."
+  end
+
+  def test_should_consider_itself_new_if_no_named_directory_exists
+    refute File.exists?(@dbname)
+    assert FileManager.new(@dbname).is_new?, 'Instance does not consider itself new.'
+  end
+
+  def test_should_not_consider_itself_new_when_named_directory_exists
+    Dir.mkdir(@dbname)
+    assert File.exists?(@dbname)
+    refute FileManager.new(@dbname).is_new?, 'Instance does not consider itself new.'
+  end
+
+  def test_should_delete_existing_temp_files_in_directory
+    Dir.mkdir(@dbname)
+    temp_filenames = 2.times.map {|i| "#{@dbname}/temp#{i}.test" }
+    FileUtils.touch(temp_filenames)
+    FileManager.new(@dbname)
+    temp_filenames.each { |f| refute File.exists?(f), "Temp file was still present in #{@dbname}" }
+  end
+
 end
