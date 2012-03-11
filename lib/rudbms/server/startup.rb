c358a046f9f1f629cdef446ce6596ff0918d2ad3
Yong Bakos <ybakos@humanoriented.com>
2012-03-11 15:49:51 -0600

    Adding server stub and simple startup test.

--- lib/rudbms/server/startup.rb
+++ lib/rudbms/server/startup.rb
@@ -1,3 +1,9 @@
+require 'rudbms/server/server'
+
 class Startup
-  
+
+  def self.main(args)
+    Server.init(args[0]);
+  end
+
 end
--- test/test_startup.rb
+++ test/test_startup.rb
@@ -1,9 +1,10 @@
-require 'rudbms/file/block'
+require 'rudbms/server/startup'
 
 class TestStartup < MiniTest::Unit::TestCase
 
-    def test
-      flunk
+    def test_main_method_should_return_boolean
+      assert_equal(!!Startup.main([]), Startup.main([]),
+                   'Startup.main did not return a boolean value')
     end
 
 end
